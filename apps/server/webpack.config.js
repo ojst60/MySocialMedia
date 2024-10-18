d25847de5eb050219627f8d219b4568181ce8a08
Julius Oyovwikigho <julius.dev1991@gmail.com>
2024-10-18 17:11:26 +0100

    fix backend pipeline

--- apps/server/webpack.config.js
+++ apps/server/webpack.config.js
@@ -2,16 +2,20 @@ const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: "./src/server",
+  entry: "./src/index.ts",
   target: "node",
   mode: process.env.NODE_ENV || "production",
   externals: [nodeExternals()],
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: "ts-loader", 
-        exclude: /node_modules/,
+        test: /\.ts$/,                   // Process TypeScript files
+        use: 'ts-loader',                // Use ts-loader for TypeScript
+        exclude: /node_modules/,         // Exclude node_modules
+      },
+      {
+        test: /\.html$/,                 // Ignore HTML files if necessary
+        use: 'ignore-loader'             // Use ignore-loader for HTML files
       },
     ],
   },
@@ -20,7 +24,19 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"), 
+    path: path.resolve(__dirname, "./dist"), 
   },
   devtool: "source-map", 
+  externals: {
+    // Exclude node modules
+    bcrypt: 'commonjs bcrypt',
+    mongodb: 'commonjs mongodb',
+    mongoose: 'commonjs mongoose',
+    express: 'commonjs express',
+    'node-gyp': 'commonjs node-gyp',
+    'aws-sdk': 'commonjs aws-sdk',
+    'mock-aws-s3': 'commonjs mock-aws-s3',
+    nock: 'commonjs nock',
+    // Add other modules as necessary
+  },
 };
